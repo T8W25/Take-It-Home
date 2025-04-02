670b8afc0574e1624fcc5edb1e9742db30c64102
sohanadhikari65 <158545932+sohanadhikari65@users.noreply.github.com>
2025-04-02 11:07:04 -0500

    Update notification.controller.js

--- TakeItHome_backend/controllers/notification.controller.js
+++ TakeItHome_backend/controllers/notification.controller.js
@@ -1,13 +1,8 @@
+// notification.controller.js
 const sendNotificationToAdmin = async (report) => {
     // Logic for sending notifications (e.g., email, socket, etc.)
     console.log(`Admin notified about the ${report.reportType} report.`);
   };
   
-  const createReport = async (data) => {
-    const newReport = new Report(data);
-    await newReport.save();
-    sendNotificationToAdmin(newReport);
-  };
-  
-  module.exports = { createReport };
+  module.exports = { sendNotificationToAdmin };
   
