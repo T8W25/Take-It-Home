bda1dbdefee971695e47ec823d4c785e69e8c3ca
JoshiDikesh21 <69516535+JoshiDikesh21@users.noreply.github.com>
2025-04-04 13:22:40 -0400

    Revert "Backend API is done for Donation Item"
    This reverts commit 4dcb784a74c889453b62f0fb98329f17563f005a.

--- TakeItHome_backend/routes/reportRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { reportItem } = require('../controllers/reportController');
-const { verifyToken } = require('../middleware/authMiddleware');  // Import the verifyToken middleware
-
-// Protect the /report route by using the verifyToken middleware
-router.post('/report', verifyToken, reportItem);
-
-module.exports = router;
