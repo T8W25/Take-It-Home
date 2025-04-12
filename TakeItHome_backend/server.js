10234182d79fc7004320f257223e1385b7f91f4b
Pratik Nepal <pratiknepal799@gmail.com>
2025-04-12 17:04:02 -0400

    Update server.js

--- TakeItHome_backend/server.js
+++ TakeItHome_backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // ✅ Match frontend
+    origin: "http://localhost:5173" || "https://take-it-home-1.onrender.com/", // ✅ Match frontend
     methods: ["GET", "POST"]
   }
 });
@@ -22,7 +22,7 @@ const MONGO_URI = process.env.MONGO_URI;
 
 // ✅ Middleware
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "http://localhost:5173" || "https://take-it-home-1.onrender.com/",
   credentials: true,
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type,Authorization"
